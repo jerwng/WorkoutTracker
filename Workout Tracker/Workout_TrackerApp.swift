c724439836466ed7290612ab0408859ba0a26f70
jerry <chenyuan.wang@mail.utoronto.ca>
2023-04-04 22:20:46 -0400

    Initial Commit

---
+++ Workout Tracker/Workout_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_TrackerApp.swift
+//  Workout Tracker
+//
+//  Created by Jerry  on 2023-04-04.
+//
+
+import SwiftUI
+
+@main
+struct Workout_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
