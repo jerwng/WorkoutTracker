278a49a922e07c20db54c3f7acaeae91f29b4007
jerry <chenyuan.wang@mail.utoronto.ca>
2023-05-28 16:26:10 -0400

    Add additional mock data to Days

--- Workout Tracker/Mock Data/Cycle/Days.swift
+++ Workout Tracker/Mock Data/Cycle/Days.swift
@@ -16,6 +16,8 @@ struct Day: Identifiable {
 // Day 3 on Microcycle 1 will have different id than Day 3 on Microcycle 2
 struct Days {
     let days = [
+        1: Day(id: 1, dayName: "Day 1", exerciseSetIds: [0]),
+        2: Day(id: 2, dayName: "Day 2", exerciseSetIds: [1]),
         3: Day(id: 3, dayName: "Day 3", exerciseSetIds: [0,1,2,3,4,5])
     ]
 }
