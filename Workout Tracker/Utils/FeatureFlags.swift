74b8dc4987b2a7b9385b922785851a240c972643
jerry <chenyuan.wang@mail.utoronto.ca>
2023-10-05 17:32:58 -0400

    Add feature flag

---
+++ Workout Tracker/Utils/FeatureFlags.swift
@@ -0,0 +1,12 @@
+//
+//  FeatureFlags.swift
+//  Workout Tracker
+//
+//  Created by Jerry  on 2023-10-05.
+//
+
+import Foundation
+
+struct FeatureFlags {
+    static let mesocycleDashboard = true
+}
