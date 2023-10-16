e88a2aced4cc386901d80d6637e98348f06675d5
jerry <chenyuan.wang@mail.utoronto.ca>
2023-10-16 16:28:03 -0400

    Add create and delete methods to Microcycle extension

--- Workout Tracker/Data/EntityExtensions/Microcycle+Extension.swift
+++ Workout Tracker/Data/EntityExtensions/Microcycle+Extension.swift
@@ -8,7 +8,7 @@
 import Foundation
 import CoreData
 
-extension Microcycle {
+extension Microcycle: EntityWithSequence {
     var microcycleName: String {
         return "\(name ?? "Unknown Microcycle")"
     }
@@ -19,5 +19,36 @@ extension Microcycle {
         return daysSet.sorted {
             $0.sequence < $1.sequence
         }
+    }
+    
+    // Static func since Microcycle won't be defined during creation
+    static func create(context: NSManagedObjectContext) -> Microcycle? {
+        let highestMicrocycleSequenceFetchRequest: NSFetchRequest<Microcycle> = NSFetchRequest(entityName: "Microcycle")
+        let highestMicrocycleSequence = EntityUtils().getEntityHighestSequence(
+            context: context,
+            fetchRequest: highestMicrocycleSequenceFetchRequest
+        )
+
+        // Highest microcycle sequence returns -1 if error occured fetching highest sequence value
+        // Prevent creating new mesocycle to prevent corrupting sequence order
+        if (highestMicrocycleSequence < 0) {
+            return nil
+        }
+        
+        let newMicrocycle = Microcycle(context: context)
+        newMicrocycle.id = UUID()
+        newMicrocycle.name = "Microcycle \(highestMicrocycleSequence + 1)"
+        newMicrocycle.sequence = Int16(highestMicrocycleSequence + 1)
+        
+        try? context.save()
+        
+        return newMicrocycle
+    }
+    
+    func delete() {
+        let context = managedObjectContext
+        context?.delete(self)
+        
+        try? context?.save()
     }
 }
