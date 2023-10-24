b905bbf9bbdebaec2568c8229c07c94a8133261e
jerry <chenyuan.wang@mail.utoronto.ca>
2023-10-24 14:02:58 -0400

    Create Exercise entity extension

---
+++ Workout Tracker/Data/EntityExtensions/Exercise+Extension.swift
@@ -0,0 +1,44 @@
+//
+//  Exercise+Extension.swift
+//  Workout Tracker
+//
+//  Created by Jerry  on 2023-10-24.
+//
+
+import Foundation
+import CoreData
+
+extension Exercise {
+    var exerciseName: String {
+        return "\(name ?? "Unknown Exercise")"
+    }
+    
+    var exerciseNotes: String {
+        return "\(notes ?? "")"
+    }
+    
+    static func create(
+        context: NSManagedObjectContext,
+        name: String,
+        notes: String,
+        repRangeTop: Int16,
+        repRangeBot: Int16
+    ) -> Exercise? {
+            
+        let newExercise = Exercise(context: context)
+        newExercise.name = name
+        newExercise.notes = notes
+        newExercise.repRangeTop = repRangeTop
+        newExercise.repRangeBot = repRangeBot
+        
+        do {
+            try context.save()
+            return newExercise
+
+        } catch {
+            print("Error creating Exercise \(name): \(error)")
+        }
+        
+        return nil
+    }
+}
